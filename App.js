7d8d9694e0e602e13bcf821494744c0b34e22437
serenitytomorrow <64203329+serenitytomorrow@users.noreply.github.com>
2020-06-02 15:30:27 -0400

    React compatibility fix

--- App.js
+++ App.js
@@ -1,104 +1,148 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import 'react-native-web';
-/*import {vibrate} from './utils'*/
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
-class DisplayTimer extends React.Component{
 
-    shouldComponentUpdate(nextProps, nextState) {
-        if(nextProps.minutes !== this.props.minutes || (nextProps.seconds !== this.props.seconds)) return true
-        return false
-    }
+class DisplayTimer extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    if (
+      nextProps.minutes !== this.props.minutes ||
+      nextProps.seconds !== this.props.seconds
+    )
+      return true;
+    return false;
+  }
 
-    render(){
-        console.log(this.props.minutes.toString() + '' + this.props.seconds.toString() + 'displayrender')
-        let textTime
-        if(this.props.minutes < 10) textTime = '0' + this.props.minutes.toString()
-        else if (this.props.minutes === 0) textTime = '00'
-        else textTime = this.props.minutes.toString()
-        textTime += ':'
-        console.log(textTime)
-        if(this.props.seconds < 10) textTime += '0' + this.props.seconds.toString()
-        else if (this.props.seconds === 0) textTime += '00'
-        else textTime += this.props.seconds.toString()
+  render() {
+    let textTime;
+    if (this.props.minutes < 10) textTime = '0' + this.props.minutes.toString();
+    else if (this.props.minutes === 0) textTime = '00';
+    else textTime = this.props.minutes.toString();
+    textTime += ':';
 
-        return (
-            <Text>!Pomodoro Timer: {textTime}</Text>
-        );
-    }
+    if (this.props.seconds < 10)
+      textTime += '0' + this.props.seconds.toString();
+    else if (this.props.seconds === 0) textTime += '00';
+    else textTime += this.props.seconds.toString();
+
+    return (
+      <Text style={{ fontSize: 26, color: '#000' }}>
+        Pomodoro Timer!:{textTime}
+      </Text>
+    );
+  }
 }
 
 export default class App extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        timer: {minutes: 25, seconds: 0},
-        countermode: 'stop',
-        counterminutes: {minutes: 25, seconds: 0},
-        timerLoop: ''
+  constructor(props) {
+    super(props);
+    this.state = {
+      timer: { minutes: 25, seconds: 0 },
+      countermode: 'stop',
+      counterminutes: { minutes: 25, seconds: 0 },
+      timerLoop: '',
+    };
+    this.handleClickStart = this.handleClickStart.bind(this);
+    this.handleClickReset = this.handleClickReset.bind(this);
+    this.handleClickStop = this.handleClickStop.bind(this);
+    this.counter = this.counter.bind(this);
+    this.handleClickToggleLength = this.handleClickToggleLength.bind(this);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    if (
+      nextState.timer !== this.state.timer ||
+      nextState.counterminutes !== this.state.counterminutes
+    )
+      return true;
+    return false;
+  }
+
+  counter() {
+    if ((this.state.timer.seconds === this.state.timer.minutes) === 0) {
+      /*Vibration.vibrate()*/
+      this.handleClickStop();
+    } else {
+      if (this.state.timer.seconds !== 0) {
+        this.setState((prevState, props) => ({
+          timer: {
+            seconds: prevState.timer.seconds - 1,
+            minutes: prevState.timer.minutes,
+          },
+        }));
+      } else {
+        this.setState((prevState, props) => ({
+          timer: { seconds: 59, minutes: prevState.timer.minutes - 1 },
+        }));
       }
-      this.handleClickStart = this.handleClickStart.bind(this);
-      this.handleClickReset = this.handleClickReset.bind(this);
-      this.handleClickStop = this.handleClickStop.bind(this);
-      this.counter = this.counter.bind(this);
-      this.handleClickToggleLength = this.handleClickToggleLength.bind(this);
     }
-    shouldComponentUpdate(nextProps, nextState) {
-        if(nextState.timer!==this.state.timer || nextState.counterminutes !== this.state.counterminutes) return true
-        return false
-    }
+  }
+  handleClickStart(e) {
+    this.setState(() => ({ countermode: 'start' }));
+    this.setState({ timerLoop: setInterval(this.counter, 1000) });
+  }
+  handleClickReset() {
+    this.setState(() => ({ countermode: 'stop' }));
+    clearTimeout(this.state.timerLoop);
+    this.setState((prevState) => ({ timer: prevState.counterminutes }));
+  }
+  handleClickStop() {
+    this.setState({ countermode: 'stop' });
+    clearTimeout(this.state.timerLoop);
+  }
+  handleClickToggleLength() {
+    this.handleClickStop();
+    if (this.state.counterminutes.minutes === 25)
+      this.setState({ counterminutes: { minutes: 5, seconds: 0 } });
+    else this.setState({ counterminutes: { minutes: 25, seconds: 0 } });
+    this.handleClickReset();
+    this.render();
+  }
 
-    counter(){
-        if(this.state.timer.seconds ===  0 && this.state.timer.minutes === 0){
-            console.log(this.state.timer.minutes+' stopcouinter')
-
-            /*vibrate()*/
-            this.handleClickStop();
-        }
-        else {
-            console.log('counter')
-            if( this.state.timer.seconds !== 0 ){
-                this.setState((prevState, props) => ({timer: {seconds: prevState.timer.seconds-1, minutes: prevState.timer.minutes}}))
-            }
-            else {
-                this.setState((prevState, props) => ({timer: {seconds: 59, minutes: prevState.timer.minutes-1}}))
-            }
-        }
-    }
-    handleClickStart(e) {
-        this.setState(() => ({countermode: 'start'}));
-        this.setState({timerLoop: setInterval(this.counter, 1000)});
-    }
-    handleClickReset() {console.log(this.state.timerLoop+' '+this.state.counter); console.log('reset');
-        this.setState(() => ({countermode: 'stop'}))
-        clearTimeout(this.state.timerLoop);
-        this.setState((prevState) => ({timer: prevState.counterminutes}))
-    }
-    handleClickStop() {console.log('stop')
-        this.setState({countermode: 'stop'})
-        clearTimeout(this.state.timerLoop);
-    }
-    handleClickToggleLength() {
-        this.handleClickStop()
-        if(this.state.counterminutes.minutes === 25) this.setState({counterminutes: {minutes: 5, seconds: 0}})
-        else this.setState({counterminutes: {minutes: 25, seconds: 0}})
-        this.handleClickReset() 
-        this.render()
-    }
-
-    render() {console.log('mainrender')
-        const buttonLength25 = <button onClick={this.handleClickToggleLength}>5 minutes/<b>25minutes</b></button>
-        const buttonLength5 = <button onClick={this.handleClickToggleLength}><b>5 minutes</b>/25minutes</button>
-        return (<div>
-          <View style={styles.container}>{console.log(this.state.timer.minutes.toString()+' '+this.state.timer.seconds.toString())}
-                <DisplayTimer minutes={this.state.timer.minutes} seconds={this.state.timer.seconds} />
-                <button onClick={this.handleClickStart}>start</button>
-                <button onClick={this.handleClickStop}>stop</button>
-                <button onClick={this.handleClickReset}>reset</button>
-                {this.state.counterminutes.minutes === 25 ? buttonLength25 : buttonLength5}
-          </View></div>
-        );
-    }
+  render() {
+      const buttonLength25 = (
+        <TouchableOpacity
+          style={{ backgroundColor: 'blue' }}
+          onClick={this.handleClickToggleLength}>
+          <Text style={{ fontSize: 10, color: '#fff' }}>
+            5 minutes/</Text><Text style={{ fontSize: 20, color: '#fff' }}>25minutes
+          </Text>
+        </TouchableOpacity>
+      );
+      const buttonLength5 = (
+        <TouchableOpacity
+          style={{ backgroundColor: 'blue' }}
+          onClick={this.handleClickToggleLength}>
+          <Text style={{ fontSize: 20, color: '#fff' }}>
+            5 minutes</Text>/<Text style={{ fontSize: 10, color: '#fff' }}>25minutes
+          </Text>
+        </TouchableOpacity>
+      );
+    return (
+      <View style={styles.container}>
+        <DisplayTimer
+          minutes={this.state.timer.minutes}
+          seconds={this.state.timer.seconds}
+        />
+        <TouchableOpacity
+          style={{ backgroundColor: 'blue' }}
+          onClick={this.handleClickStart}>
+          <Text style={{ fontSize: 20, color: '#fff' }}>start</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{ backgroundColor: 'blue' }}
+          onClick={this.handleClickStop}>
+          <Text style={{ fontSize: 20, color: '#fff' }}>stop</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{ backgroundColor: 'blue' }}
+          onClick={this.handleClickReset}>
+          <Text style={{ fontSize: 20, color: '#fff' }}>reset</Text>
+        </TouchableOpacity>
+        {this.state.counterminutes.minutes === 25
+          ? buttonLength25
+          : buttonLength5}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
